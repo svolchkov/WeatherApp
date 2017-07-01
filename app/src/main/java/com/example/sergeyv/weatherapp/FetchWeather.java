bff94eba85f58c57ebe140e3ba79f09a460fe422
svolchkov <s.volchkov@yahoo.com.au>
2017-07-01 16:49:45 +0800

    x

---
+++ app/src/main/java/com/example/sergeyv/weatherapp/FetchWeather.java
@@ -0,0 +1,58 @@
+package com.example.sergeyv.weatherapp;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by sergeyv on 1/07/2017.
+ */
+
+public class FetchWeather {
+    private static final String OPEN_WEATHER_MAP_API =
+            "http://api.openweathermap.org/data/2.5/weather?q=%s&units=metric&APPID=%s";
+
+    public static JSONObject getJSON(Context context, String city) {
+        try {
+            URL url = new URL(String.format(OPEN_WEATHER_MAP_API, city,context.getString(R.string.openWeatherApiKey)));
+            HttpURLConnection connection =
+                    (HttpURLConnection) url.openConnection();
+
+//            connection.addRequestProperty("x-api-key",
+//                    context.getString(R.string.openWeatherApiKey));
+            connection.getResponseCode();
+            InputStream stream = connection.getErrorStream();
+            if (stream != null) {
+                return null;
+            }
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            StringBuffer json = new StringBuffer(1024);
+            String tmp = "";
+            while ((tmp = reader.readLine()) != null)
+                json.append(tmp).append("\n");
+            reader.close();
+
+            JSONObject data = new JSONObject(json.toString());
+
+            // This value will be 404 if the request was not
+            // successful
+            if (data.getInt("cod") != 200) {
+                return null;
+            }
+
+            return data;
+        } catch (Exception e) {
+            Log.e("MYAPP", "exception", e);
+            return null;
+        }
+    }
+}
--- app/src/main/java/com/example/sergeyv/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/sergeyv/weatherapp/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.sergeyv.weatherapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.media.audiofx.BassBoost;
+import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.design.widget.Snackbar;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,8 +19,13 @@ import android.view.MenuItem;
 import android.widget.TextView;
 import com.example.sergeyv.weatherapp.model.Settings;
 
+import org.json.JSONObject;
+
+import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
 
 import static android.content.res.Configuration.ORIENTATION_LANDSCAPE;
 import static android.support.design.widget.Snackbar.LENGTH_LONG;
@@ -30,6 +38,14 @@ public class MainActivity extends AppCompatActivity {
     int textColor;
     public static String MY_PREFS_NAME = "com.example.sergeyv.weatherapp.settings_"; // name of preferences file
 
+    //TEMP
+    TextView cityField;
+    TextView updatedField;
+    TextView detailsField;
+    TextView currentTemperatureField;
+    TextView weatherIcon;
+
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +56,15 @@ public class MainActivity extends AppCompatActivity {
         tvTitle = (TextView) findViewById(R.id.tvTitle);
         tvCity = (TextView) findViewById(R.id.tvCity);
         tvTemperature =  (TextView) findViewById(R.id.tvTemp);
+        cityField = (TextView)findViewById(R.id.city_field);
+        updatedField = (TextView)findViewById(R.id.updated_field);
+        detailsField = (TextView)findViewById(R.id.details_field);
+        currentTemperatureField = (TextView)findViewById(R.id.current_temperature_field);
+        weatherIcon = (TextView)findViewById(R.id.weather_icon);
 
-        textViews = new ArrayList<TextView>(Arrays.asList(tvDateTime,tvTitle,tvCity,tvTemperature));
+        textViews = new ArrayList<TextView>(Arrays.asList(tvDateTime,tvTitle,
+                tvCity,tvTemperature,cityField, updatedField, detailsField,
+                currentTemperatureField, weatherIcon));
 
         SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
         if (prefs != null) {
@@ -74,8 +97,64 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         final ActionBar actionBar = getSupportActionBar();
+
+
+
+        handler = new Handler();
+
+        updateWeatherData(Settings.city);
     }
 
+    private void updateWeatherData(String city){
+        final String cityName = city.split(",")[0] + ",au";
+        final Context context = this;
+        new Thread(){
+            public void run(){
+                final JSONObject json = FetchWeather.getJSON(context, cityName);
+                if(json == null){
+                    handler.post(new Runnable(){
+                        public void run(){
+                            String s = getString(R.string.unknownCity);
+                            Snackbar mySnackbar = Snackbar.make(findViewById(R.id.constraintLayout), s + ": " + cityName, Snackbar.LENGTH_LONG);
+                            mySnackbar.show();
+                        }
+                    });
+                } else {
+                    handler.post(new Runnable(){
+                        public void run(){
+                            renderWeather(json);
+                        }
+                    });
+                }
+            }
+        }.start();
+    }
+
+    private void renderWeather(JSONObject json){
+        try {
+            cityField.setText(json.getString("name").toUpperCase(Locale.UK) +
+                    ", " +
+                    json.getJSONObject("sys").getString("country"));
+
+            JSONObject details = json.getJSONArray("weather").getJSONObject(0);
+            JSONObject main = json.getJSONObject("main");
+            detailsField.setText(
+                    details.getString("description").toUpperCase(Locale.US) +
+                            "\n" + "Humidity: " + main.getString("humidity") + "%" +
+                            "\n" + "Pressure: " + main.getString("pressure") + " hPa");
+
+            currentTemperatureField.setText(
+                    String.format("%.2f", main.getDouble("temp"))+ " ℃");
+
+            DateFormat df = DateFormat.getDateTimeInstance();
+            String updatedOn = df.format(new Date(json.getLong("dt")*1000));
+            tvDateTime.setText("Last update: " + updatedOn);
+
+
+        }catch(Exception e){
+            Log.e("SimpleWeather", "One or more fields not found in the JSON data");
+        }
+    }
 
     @Override
     protected void onResume() {
@@ -88,6 +167,8 @@ public class MainActivity extends AppCompatActivity {
         if (Settings.city != null){
             tvCity.setText(Settings.city);
         }
+
+        updateWeatherData(Settings.city);
     }
 
     @Override
